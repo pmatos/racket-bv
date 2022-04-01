76a1ac6e116e3a1314663bbc70a6f3725a58a112
Paulo Matos <pmatos@linki.tools>
2022-04-01 09:26:09 +0100

    Stick with rosette v3.2 for testing
    Rosette v4 require racket v8.1 already so lets wait.
    In any case, we only need rosette for testing, so it's
    not urgent.

--- info.rkt
+++ info.rkt
@@ -5,7 +5,13 @@
 (define collection "bv")
 
 (define deps '("base" "mischief" "math-lib"))
-(define build-deps '("sandbox-lib" "scribble-lib" "racket-doc" "rackunit-lib" "quickcheck" "rosette"))
+(define build-deps
+  '("sandbox-lib"
+    "scribble-lib"
+    "racket-doc"
+    "rackunit-lib"
+    "quickcheck"
+    ("rosette" #:version "3.2")))
 
 (define scribblings '(("scribblings/bv.scrbl" ())))
 (define pkg-desc "Bitvector library following a Rosette-like API")
